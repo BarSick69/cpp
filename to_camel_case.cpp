ffa9a5b6cc569ee79eee4898a9f6a066b94f9c5c
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2022-11-15 22:42:51 +0200

    Update to_camel_case.cpp

--- to_camel_case.cpp
+++ to_camel_case.cpp
@@ -2,21 +2,41 @@
 #include <iostream>
 #include <string>
 using namespace std;
-string to_camel_case(string text) {
+
+std::string to_camel_case(std::string text) {
   int length=text.length();
+  int new_len=length;
+  char temp=' ';
   for (int i=0;i<length-1;i++){
     if((text[i]=='-'||text[i]=='_')){
       if(int(text[i+1])<=122 && int(text[i+1])>=97){
         text[i]=char(int(text[i+1])-32);
+        cout<<text<<endl;
+        for(int k=i+1;k<new_len+k-(k+1);k++){
+            text[k]=text[k+1];
+        }
+        text.pop_back();
+        cout<<"Text dupa for:"<<text<<endl;
+        
+        new_len-=1;
+      }
+       else if(int(text[i+1])<=90&&int(text[i+1])>=65){
+        cout<<"HI";
+        text[i]=text[i+1];
+        for(int k=i+1;k<new_len+k-(k+1);k++){
+            text[k]=text[k+1];
+        }
+        text.pop_back();
+        new_len-=1;
       }
     }
-  }
+    }
   return text;
 }
 int main() {
     // Write C++ code here
     std::cout << "Hello world!";
-    cout<<to_camel_case("a-a_s_sf_g_as-ag");
+    cout<<to_camel_case("A_Cat_Is-pippi");
 
     return 0;
 }
