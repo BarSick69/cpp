f2e431f700b3e844fd37ba77e648119fcbbd9f80
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2022-11-15 22:47:40 +0200

    Update to_camel_case.cpp

--- to_camel_case.cpp
+++ to_camel_case.cpp
@@ -2,26 +2,29 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 std::string to_camel_case(std::string text) {
   int length=text.length();
   int new_len=length;
-  char temp=' ';
+  //go through the string
   for (int i=0;i<length-1;i++){
+    //check for _ and -
     if((text[i]=='-'||text[i]=='_')){
+      //check if the char after the symbol is a lowercase char
       if(int(text[i+1])<=122 && int(text[i+1])>=97){
+        //change the symbol to uppercase char
         text[i]=char(int(text[i+1])-32);
-        cout<<text<<endl;
+        //move the text to the left
         for(int k=i+1;k<new_len+k-(k+1);k++){
             text[k]=text[k+1];
         }
+        //remove the last char
         text.pop_back();
-        cout<<"Text dupa for:"<<text<<endl;
-        
+        //change the len of the string because you have removed a character
         new_len-=1;
       }
+      //otherwise check if the char after the symbol is a uppercase char
        else if(int(text[i+1])<=90&&int(text[i+1])>=65){
-        cout<<"HI";
+        //Same process as from line 14-22
         text[i]=text[i+1];
         for(int k=i+1;k<new_len+k-(k+1);k++){
             text[k]=text[k+1];
